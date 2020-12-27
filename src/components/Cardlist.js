ccabd5360e9296f4e8798751b65a494c6e761c70
SeifESlimene <s.slimene19@gmail.com>
2020-12-27 12:21:53 +0100

    trying to solve the state

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "antd";
 import Singlecard from "./Singlecard";
-
 
 function randomPic() {
   let tabIds = [];
@@ -22,16 +21,70 @@ for (let i = 0; i <= random.length; i += 5) {
   matrix.push(random.slice(i, i + 5));
 }
 
+let i = 0;
+let checkArray = [];
+let style = [];
 
 function Cardlist() {
+  const [isFlipped, setIsFlipped] = useState(true);
+  const [cards, setCards] = useState([
+    {
+      row: 0,
+      column: 0,
+      isFlipped: false,
+    },
+    {
+      row: 0,
+      column: 1,
+      isFlipped: false,
+    },
+  ]);
+  const handleClick = (row, column) => {
+    console.log("Row Is " + row);
+    console.log("Column Is " + column);
+    // if (i < 2) {
+    //   i++;
+    //   style.push(e.target.parentElement.style);
+    //   checkArray.push(e.target.dataset.url);
+      // setIsFlipped(!isFlipped);
+      // if (checkArray.length === 2) {
+      //   if (checkArray[0] === checkArray[1]) {
+      //     style[0].display = "none";
+        //   style[1].display = "none";
+        //   i = 0;
+        //   style = [];
+        //   checkArray = [];
+        // } else {
+        //   console.log("Not Equal");
+        //   i = 0;
+        //   checkArray = [];
+        //   style = [];
+          // setTimeout(() => {
+          //   setIsFlipped(false);
+          // }, 1000);
+    //     }
+    //   }
+    // }
+  };
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsFlipped(!isFlipped);
+  //   }, 3000);
+  // }, []);
   return (
     <div>
-      {matrix.map((row, key) => (
-        <Row key={key} gutter={[16, 16]}>
-          {row.map((column, key) => {
+      {matrix.map((row, indexRow) => (
+        <Row key={indexRow} gutter={[16, 16]}>
+          {row.map((column, indexColumn) => {
             return (
-              <Col key={key} span={4}>
-                <Singlecard url={column} />
+              <Col key={indexColumn} span={4}>
+                <Singlecard
+                  row={indexRow + 1}
+                  column={indexColumn + 1}
+                  handleClick={handleClick}
+                  // isFlipped={isFlipped}
+                  url={column}
+                />
               </Col>
             );
           })}
--- src/components/Singlecard.js
+++ src/components/Singlecard.js
@@ -1,43 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { Image } from "antd";
 
-let i = 0;
-let checkArray = [];
-let style = [];
+function SingleCard({ url, id, isFlipped, handleClick, row, column }) {
 
-function SingleCard({ url, id }) {
-  const [flipped, setFlipped] = useState(true);
-  const handleClick = (e) => {
-    if (i < 2) {
-
-      i++;
-      style.push(e.target.parentElement.style);
-      checkArray.push(e.target.dataset.url);
-      setFlipped(!flipped);
-      if (checkArray.length === 2) {
-        if (checkArray[0] === checkArray[1]) {
-          style[0].display = "none";
-          style[1].display = "none";
-          i = 0;
-          style = [];
-          checkArray = [];
-        } else {
-          console.log("Not Equal");
-          i = 0;
-          checkArray = [];
-          style = [];
-          setTimeout(() => {
-            setFlipped(false);
-          }, 1000);
-        }
-      }
-    }
-  };
-  useEffect(() => {
-    setTimeout(() => {
-      setFlipped(!flipped);
-    }, 3000);
-  }, []);
   return (
     <>
       <div
@@ -51,7 +16,7 @@ function SingleCard({ url, id }) {
           color: "#fff",
         }}
       >
-        {flipped ? (
+        {isFlipped ? (
           <Image height="80" width="80" src={url} preview={false} />
         ) : (
           <span
@@ -63,9 +28,7 @@ function SingleCard({ url, id }) {
               justifyContent: "center",
             }}
             data-url={url}
-            onClick={(e) => {
-              return handleClick(e);
-            }}
+            onClick={() => handleClick(row, column)}
           >
             ?
           </span>
